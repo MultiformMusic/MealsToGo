f823e760add49151e3c631cf06b8896e121c3822
mdio <michel.dio33@gmail.com>
2021-03-11 16:56:47 +0100

    Restaurant Card

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Card } from "react-native-paper";
+import styled from "styled-components/native";
+
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const Title = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body}
+  padding: ${(props) => props.theme.space[3]};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://media-cdn.tripadvisor.com/media/photo-s/18/b4/4c/4f/notre-restaurant.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosesTemporarily,
+  } = restaurant;
+
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </RestaurantCard>
+  );
+};
